dd9c309140432477b2a27f8e1c0557b5c4d3c84d
GigaMegaOrphan <id25.a.lemoss@vtdt.edu.lv>
2026-04-17 15:51:50 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,110 @@
+
+
+
+let dogFact = document.getElementById("Fact");
+let foxFact = document.getElementById("Fact");
+let catFact = document.getElementById("Fact");
+let on = false;
+let which = 0;
+let constant = 0;
+let lever;
+let isOnCooldown = false;
+
+async function fetchDogData(){
+
+    if (isOnCooldown) return;
+    isOnCooldown = true;
+
+    try{
+        const response = await fetch(`https://dog.ceo/api/breeds/image/random`);
+
+        const data = await response.json();
+        const cutePic = data.message;
+        const imgElement = document.getElementById("cutePic");
+
+        imgElement.src = cutePic;
+
+        dogFact.innerHTML = ""
+
+        fetch(`https://dogapi.dog/api/facts`)
+        .then(response => response.json())
+        .then(data => {dogFact.innerHTML += `${data.facts}`});
+
+    }
+    catch(error){
+        console.error(error);
+    }
+
+    document.getElementById("cdfact").innerHTML = "Dog fact:"
+
+    setTimeout(function(){
+        isOnCooldown = false;
+    }, 1000);
+}
+
+
+async function fetchCatData() {
+
+    if (isOnCooldown) return;
+    isOnCooldown = true;
+
+    try{
+        const response = await fetch(`https://api.thecatapi.com/v1/images/search`);
+
+        const data = await response.json();
+        const cutePic = data[0].url;
+        const imgElement2 = document.getElementById("cutePic");
+
+        imgElement2.src = cutePic;
+
+
+        dogFact.innerHTML = ""
+
+        fetch(`https://catfact.ninja/fact?max_length=140`)
+        .then(response => response.json())
+        .then(data => {catFact.innerHTML += `${data.fact}`});
+
+    }
+    catch(error){
+        console.error(error);
+    }
+
+    document.getElementById("cdfact").innerHTML = "Cat fact:";
+
+    setTimeout(function(){
+        isOnCooldown = false;
+    }, 1000);
+}
+
+function autoScroll() {
+
+    console.log("clicked");
+    lever = document.getElementById("autoScrollText");
+
+    if(on === false){
+        on = true;
+        lever.style.color = "white";
+        lever.style.background = "hsl(187, 100%, 47%)";
+        document.getElementById("autoScrollText").innerHTML = "Auto Scroll: on";
+        constant = setInterval(function() {
+            console.log("5 seconds passed");
+            if(which === 0){
+                fetchDogData();
+                which = 1;
+            }else if(which === 1){
+                fetchCatData();
+                which = 0;
+            }
+        }, 5000);
+    }else{
+        on = false;
+        lever.style.color = "lightgray";
+        lever.style.background = "hsl(187, 100%, 39%)";
+        document.getElementById("autoScrollText").innerHTML = "Auto Scroll: off";
+        clearInterval(constant);
+    }
+}
+    
+
+
+
